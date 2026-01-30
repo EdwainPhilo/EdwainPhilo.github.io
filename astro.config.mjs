cf5e62500738be327c6ed9c1389770552dcadcfc
Philo <Edwainmoma3@gmail.com>
2026-01-30 17:51:11 +0800

    Revert "feat: 添加 Decap CMS OAuth 集成"
    This reverts commit e00c7d37528980252b22fbc46ee03fcc7fc4cca1.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,9 @@
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import {defineConfig} from 'astro/config';
-import decapCmsOauth from 'astro-decap-cms-oauth'
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://example.com',
-    integrations: [mdx(), sitemap(), decapCmsOauth()],
+    integrations: [mdx(), sitemap()],
 });
